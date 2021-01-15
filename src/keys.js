eeba9f17bdca27995f96e3e6af4e43a5a01abf95
emunera <edisonupb2013@gmail.com>
2021-01-14 20:01:24 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+    database: {
+        host: 'us-cdbr-east-03.cleardb.com',
+        user: 'be7135c7eb02c0',
+        password: '57475ea8',
+        database: 'heroku_cea3c0fd4f250a2'
+    }
+};
---
+++ src/routes/users.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const router = express.Router();
+
+const pool = require('../db'); // Primero: conectar a la base de datos
+
+router.get('/agregar', (req, res) => { // Añadir un usuario nuevo
+    res.render('users/agregar'); // Segundo: en sitio/control/agregar (Ver index).
+});
+
+router.post('/agregar', async (req, res) => {
+    const { fullname, username, password, phone, email } = req.body;
+    const newCommonUser = {
+        fullname,
+        username,
+        password,
+        phone,
+        email
+    };
+    // console.log(newCommonUser);  Comprueba que se están recibiendo los datos
+    await pool.query('INSERT INTO users SET ?', [newCommonUser]) // Tercero: guarda en la db
+    res.redirect('/control/users');
+});
+
+router.get('/users', async (req, res) => {
+   const users = await pool.query('SELECT * FROM users'); // Cinco: Consulta la db
+   // console.log(users); // comprueba que se reciben los datos
+   res.render('users/lista', {users}); // Llamar en html
+});
+
+router.get('/users/delete/:id', async (req, res) => {
+    const { id } = req.params; // Eliminar usuario
+    // console.log(id); // comprueba que se reciben los datos
+    await pool.query('DELETE FROM users WHERE id = ?', [id]);
+    res.redirect('/control/users');
+});
+
+// Editar usuario
+router.get('/users/editar/:id', async (req, res) => {
+    const { id } = req.params;
+    // console.log(id); // comprueba que se reciben los datos
+    const users = await pool.query('SELECT * FROM users WHERE id= ?', [id])
+    console.log(users[0]);
+    res.render('users/editar', {user: users[0]});
+});
+
+router.post('/users/editar/:id', async (req, res) => {
+    const { id } = req.params;
+    const { fullname, username, password, phone, email } = req.body;
+    const newCommonUser = {
+        fullname,
+        username,
+        password,
+        phone,
+        email
+    };
+    await pool.query('UPDATE users SET ? WHERE id = ?', [newCommonUser, id]);
+    res.redirect('/control/users');
+});
+
+module.exports = router;
